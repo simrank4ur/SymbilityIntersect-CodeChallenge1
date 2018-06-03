d9a6702ae5faba48228f9ac8f062b984e3b10bce
Simran Kaur <simrankaur1224@gmail.com>
2018-06-03 18:17:26 -0400

    Splash screen working

---
+++ app/src/main/java/iameskay/com/cryptocharts/SplashActivity.java
@@ -0,0 +1,25 @@
+package iameskay.com.cryptocharts;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 2000);
+    }
+}
